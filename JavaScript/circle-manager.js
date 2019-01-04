6bcc306896f314a026e82872a0ae2185e3d3014e
Evan Wills <evan.wills@acu.edu.au>
2019-01-04 23:14:41 +1100

    apply StandardJS code styling.

--- JavaScript/circle-manager.js
+++ JavaScript/circle-manager.js
@@ -1,12 +1,12 @@
 // ==================================================================
 // How it should work:
-//		1.
-//		2.	for each child,
-//		2.a.	get radiusPointX & radiusPointY
-//		2.b.	rotate radiusPointX & radiusPointY around current
-//				orginX & orginY
-//		2.c.	send new radiusPointX & radiusPointY back to child
-//		3.	repeat steps 1 - 3 for each child
+// 1.
+// 2. for each child,
+//    a. get radiusPointX & radiusPointY
+//    b. rotate radiusPointX & radiusPointY around current
+//       orginX & orginY
+//    c. send new radiusPointX & radiusPointY back to child
+// 3. repeat steps 1 - 3 for each child
 
 // ==================================================================
 // START: circleManager family
@@ -20,361 +20,284 @@
 // this.childCircle.this.circle.this.originX should all share the same pointer.
 // The same goes for the this.radiusPointY
 
-function CircleManager() {
-	'use strict';
-	this.circle = null;
-	this.depth = 0;
+function CircleManager () {
+  'use strict'
+  this.circle = null
+  this.depth = 0
 }
 
-CircleManager.prototype = new CircleInterface();
-CircleManager.prototype.constructor = CircleManager;
+CircleManager.prototype = new CircleInterface()
+CircleManager.prototype.constructor = CircleManager
 /**
- * @method	depthOK() is used to varify that the stack of
- *			circles is OK. By OK, I mean that the
- *					last circle is a singleCircle object and
- *					that no MultiCircle objects were added
- *					after it.
+ * @function depthOK() is used to varify that the stack of circles is
+ *         OK. By OK, I mean that the last circle is a singleCircle
+ *         object and that no MultiCircle objects were added after it.
  *
- * This method should be called after all the circleManager
- * objects have been initialised.
+ * This method should be called after all the circleManager objects
+ * have been initialised.
  *
- * NOTE: Because of the way MultiCircle is setup, it should
- *		 be OK to use a stack even if the output of depthOK()
- *		 is FALSE.
+ * NOTE: Because of the way MultiCircle is setup, it should be OK to
+ *       use a stack even if the output of depthOK() is FALSE.
+ * @returns boolean
  */
 CircleManager.prototype.depthOK = function () {
-	'use strict';
-	return true;
-};
-
+  'use strict'
+  return true
+}
 CircleManager.prototype.fixDepth = function () {
-	'use strict';
-	return true;
-};
-
+  'use strict'
+  return true
+}
 /**
- * @method	setChildCircle() adds another circleManager
- *			object to the top of the stack
+ * @function setChildCircle() adds another circleManager
+ *               object to the top of the stack
  */
 CircleManager.prototype.setChildCircle = function (childCircle) {
-	'use strict';
-};
-
+  'use strict'
+}
 /**
- * @method	setRadiusPointXY() sets the radius point at a
- *			given depth after the XY points have been rotated
- *			by lower circleManager object
+ * @function setRadiusPointXY() sets the radius point at a
+ *               given depth after the XY points have been rotated
+ *               by lower circleManager object
  *
  * @param [double]   x         the X coordinate of the radius point
  * @param [double]   y         the Y coordinate of the radius point
  * @param [unsigned  in]       depth how far from the top of the
- *			                          stack the radius point should be set
+ *                                         stack the radius point should be set
  */
 CircleManager.prototype.setRadiusPointXY = function (x, y, depth) {
-	'use strict';
-
-};
-
+  'use strict'
+}
 /**
- * @method	getCircleShape() returns the circleShape object
- *			used at a given depth;
+ * @function getCircleShape() returns the circleShape object
+ *               used at a given depth
  */
 CircleManager.prototype.getCircleShape = function (depth) {
-	'use strict';
-	return this;
-};
-
+  'use strict'
+  return this
+}
 /**
- * @method getDepth() returns the level in the stack that the
- *					circle is at
- *					e.g. if there are 5 circles in the stack
- *						 and this one was added fourth, it's
- *						 depth would be 1
+ * @function getDepth() returns the level in the stack that the
+ *           circle is at
+ *           e.g. if there are 5 circles in the stack and this one
+ *                was added fourth, it's depth would be 1
  */
 CircleManager.prototype.getDepth = function () {
-	'use strict';
-	return 0;
-};
-
+  'use strict'
+  return 0
+}
 /**
- * @method	getRadiusPointX() returns the this.radiusPointX
- *			coordinate at a given depth
+ * @function getRadiusPointX() returns the this.radiusPointX
+ *               coordinate at a given depth
  */
 CircleManager.prototype.getRadiusPointX = function (depth) {
-	'use strict';
-	return 0;
-};
-
+  'use strict'
+  return 0
+}
 /**
- * @method	getRadiusPointY() like getRadiusPointX() but for
- *			the Y value
+ * @function getRadiusPointY() like getRadiusPointX() but for
+ *               the Y value
  */
 CircleManager.prototype.getRadiusPointY = function (depth) {
-	'use strict';
-	return 0;
-};
-
-
-
-
-
-
-
+  'use strict'
+  return 0
+}
 //  END:  CircleManager (interface)
 // ==================================================================
 // START: SingleCircle
 
-
-
-
-
-
-
-function SingleCircle(circle) {
-	'use strict';
-	if (typeof circle !== 'CircleShape') {
-		throw {'msg': 'SingleCircle expects only parameter to be a CircleShape object. ' + typeof circle + ' given'};
-	}
-	this.circle = circle;
+function SingleCircle (circle) {
+  'use strict'
+  if (typeof circle !== 'CircleShape') {
+    throw Error('SingleCircle expects only parameter to be a CircleShape object. ' + typeof circle + ' given')
+  }
+  this.circle = circle
 }
 
-SingleCircle.prototype =  new CircleManager();
-SingleCircle.prototype = Object.create(CircleManager);
-
+SingleCircle.prototype = new CircleManager()
+SingleCircle.prototype = Object.create(CircleManager)
 SingleCircle.prototype.setChildCircle = function (childCircle) {
-	'use strict';
-	// do nothing singleCirclePoint objects have no child
-};
-
+  'use strict'
+  // do nothing singleCirclePoint objects have no child
+}
 SingleCircle.prototype.setRadiusPointXY = function (x, y, depth) {
-	'use strict';
-	this.radiusPointX = x;
-	this.radiusPointY = y;
-};
-
+  'use strict'
+  this.radiusPointX = x
+  this.radiusPointY = y
+}
 SingleCircle.prototype.setOriginXY = function (x, y) {
-	'use strict';
-	this.OriginX = x;
-	this.OriginY = y;
-	this.circle.setOriginXY(x, y);
-};
-
+  'use strict'
+  this.OriginX = x
+  this.OriginY = y
+  this.circle.setOriginXY(x, y)
+}
 SingleCircle.prototype.initXY = function (x, y) {
-	'use strict';
-	this.setOriginXY(x, y);
-	this.circle.initXY(x, y);
-	this.radiusPointX = this.circle.getX();
-	this.radiusPointY = this.circle.getY();
-};
-
+  'use strict'
+  this.setOriginXY(x, y)
+  this.circle.initXY(x, y)
+  this.radiusPointX = this.circle.getX()
+  this.radiusPointY = this.circle.getY()
+}
 /**
- * @method	move() rotates its own radius points
+ * @function move() rotates its own radius points
  */
 SingleCircle.prototype.move = function () {
-	'use strict';
-	this.circle.setOriginXY(this.originX, this.originY);
-	this.circle.rotateXY(this.radiusPointX, this.radiusPointY);
-	this.radiusPointX = this.circle.getX();
-	this.radiusPointY = this.circle.getY();
-};
-
+  'use strict'
+  this.circle.setOriginXY(this.originX, this.originY)
+  this.circle.rotateXY(this.radiusPointX, this.radiusPointY)
+  this.radiusPointX = this.circle.getX()
+  this.radiusPointY = this.circle.getY()
+}
 SingleCircle.prototype.getAngleStep = function (depth) {
-	'use strict';
-	return this.circle.getAngleStep(depth);
-};
-
+  'use strict'
+  return this.circle.getAngleStep(depth)
+}
 SingleCircle.prototype.getRadiusPointX = function (depth) {
-	'use strict';
-	return this.radiusPointX;
-};
-
+  'use strict'
+  return this.radiusPointX
+}
 SingleCircle.prototype.getRadiusPointY = function (depth) {
-	'use strict';
-	return this.radiusPointY;
-};
-
-
-
-
-
-
-
+  'use strict'
+  return this.radiusPointY
+}
 //  END:  SingleCircle
 // ==================================================================
 // START: MultiCircle
 
-
-
-
-
-
-
-function MultiCircle(circle) {
-	'use strict';
-
-	if (typeof circle !== 'CircleShape') {
-		throw {'msg': 'SingleCircle expects only parameter to be a CircleShape object. ' + typeof circle + ' given'};
-	}
-	this.circle = circle;
-
-	this.childCircle = null;
+function MultiCircle (circle) {
+  'use strict'
+  if (typeof circle !== 'CircleShape') {
+    throw Error('SingleCircle expects only parameter to be a CircleShape object. ' + typeof circle + ' given')
+  }
+  this.circle = circle
+  this.childCircle = null
 }
 
-
-MultiCircle.prototype = new SingleCircle();
-MultiCircle.prototype = Object.create(MultiCircle);
-
+MultiCircle.prototype = new SingleCircle()
+MultiCircle.prototype = Object.create(MultiCircle)
 MultiCircle.prototype.setChildCircle = function (childCircle) {
-	'use strict';
+  'use strict'
+  if (childCircle instanceof SingleCircle) { } else {
+    throw Error('MultiCircle.setChildCircle() expects only parameter "childCircle" to be either a SingleCircle or MultiCircle object. ' + childCircle.prototype + ' given.')
+  }
 
-	if (!childCircle instanceof SingleCircle) {
-		throw {'message': 'MultiCircle.setChildCircle() expects only parameter "childCircle" to be either a SingleCircle or MultiCircle object. ' + childCircle.prototype + ' given.'};
-	}
-
-	this.depth += 1;
-	if (this.childCircle ===  null) {
-		this.childCircle = childCircle;
-		this.childCircle.initXY(this.radiusPointX, this.radiusPointY);
-	} else {
-		this.childCircle.setChildCircle(this.childCircle);
-	}
-};
-
+  this.depth += 1
+  if (this.childCircle === null) {
+    this.childCircle = childCircle
+    this.childCircle.initXY(this.radiusPointX, this.radiusPointY)
+  } else {
+    this.childCircle.setChildCircle(this.childCircle)
+  }
+}
 /**
- * @method	move() rotates its own radius points as
- *			well as those of all its children. It also sets
- *			the this.originX & this.originY of its direct child
- *			manager
+ * @function move() rotates its own radius points as
+ *               well as those of all its children. It also sets
+ *               the this.originX & this.originY of its direct child
+ *               manager
  */
 MultiCircle.prototype.move = function () {
-	'use strict';
-	var tmpDepth = this.depth,
-		radiusPointX = 0,
-		radiusPointY = 0;
+  'use strict'
+  var tmpDepth = this.depth
+  var radiusPointX = 0
+  var radiusPointY = 0
+  this.circle.setOriginXY(this.originX, this.originY)
+  for (tmpDepth; tmpDepth >= 0; tmpDepth -= 1) {
+    radiusPointX = this.childCircle.getRadiusPointX(tmpDepth)
+    radiusPointY = this.childCircle.getRadiusPointY(tmpDepth)
+    this.circle.rotateXY(radiusPointX, radiusPointY)
+    this.childCircle.setRadiusPointXY(this.circle.getX(), this.circle.getY(), tmpDepth)
+  }
 
-	this.circle.setOriginXY(this.originX, this.originY);
-
-	for (tmpDepth; tmpDepth >= 0; tmpDepth -= 1) {
-
-		radiusPointX = this.childCircle.getRadiusPointX(tmpDepth);
-		radiusPointY = this.childCircle.getRadiusPointY(tmpDepth);
-
-		this.circle.rotateXY(radiusPointX, radiusPointY);
-
-		this.childCircle.setRadiusPointXY(this.circle.getX(), this.circle.getY(), tmpDepth);
-	}
-
-	this.circle.rotate();
-
-	this.radiusPointX = this.circle.getX();
-	this.radiusPointY = this.circle.getY();
-
-	this.childCircle.setOriginXY(this.radiusPointX, this.radiusPointY);
-	this.childCircle.rotate();
-};
-
-
+  this.circle.rotate()
+  this.radiusPointX = this.circle.getX()
+  this.radiusPointY = this.circle.getY()
+  this.childCircle.setOriginXY(this.radiusPointX, this.radiusPointY)
+  this.childCircle.rotate()
+}
 /**
- * @method depthOK() checks whether the hierarchy of nested
- *			circles is correct
+ * @function depthOK() checks whether the hierarchy of nested
+ *           circles is correct
  */
 MultiCircle.prototype.depthOK = function () {
-	'use strict';
-	var output = false;
-	if (this.childCircle === null) {
-		if (this.depth === 0) {
-			output = true;
-		}
-	} else if (this.childCircle.getDepth() + 1 === this.depth) {
-		output = true;
-	}
-	// this looks OK lets see whether the child is OK
-	return output;
-};
-
-
+  'use strict'
+  var output = false
+  if (this.childCircle === null) {
+    if (this.depth === 0) {
+      output = true
+    }
+  } else if (this.childCircle.getDepth() + 1 === this.depth) {
+    output = true
+  }
+  // this looks OK lets see whether the child is OK
+  return output
+}
 /**
- * @method fixDepth() makes sure the hierarchy of nested
- *			circles is correct
+ * @function fixDepth() makes sure the hierarchy of nested
+ *           circles is correct
  */
 MultiCircle.prototype.fixDepth = function () {
-	'use strict';
-	if (this.childCircle === null) {
-		this.depth = 0;
-	} else {
-		this.childCircle.fixDepth();
-		this.depth = this.childCircle.getDepth() + 1;
-	}
-};
-
+  'use strict'
+  if (this.childCircle === null) {
+    this.depth = 0
+  } else {
+    this.childCircle.fixDepth()
+    this.depth = this.childCircle.getDepth() + 1
+  }
+}
 MultiCircle.prototype.setRadiusPointXY = function (x, y, depth) {
-	'use strict';
-	depth -= 1;
-	if (depth === this.depth) {
-		this.radiusPointX = x;
-		this.radiusPointY = y;
-		// the radius point of this circle is the origin of the child
-		this.childCircle.setOriginXY(x, y);
-	} else {
-		this.childCircle.setRadiusPointXY(x, y, depth);
-	}
-};
-
+  'use strict'
+  depth -= 1
+  if (depth === this.depth) {
+    this.radiusPointX = x
+    this.radiusPointY = y
+    // the radius point of this circle is the origin of the child
+    this.childCircle.setOriginXY(x, y)
+  } else {
+    this.childCircle.setRadiusPointXY(x, y, depth)
+  }
+}
 MultiCircle.prototype.getAngleStep = function (depth) {
-	'use strict';
-	depth -= 1;
-	if (depth === this.depth) {
-		return this.circle.getAngleStep();
-	} else {
-		return this.childCircle.getAngleStep(depth);
-	}
-};
-
-
+  'use strict'
+  depth -= 1
+  if (depth === this.depth) {
+    return this.circle.getAngleStep()
+  } else {
+    return this.childCircle.getAngleStep(depth)
+  }
+}
 MultiCircle.prototype.getRadiusPointX = function (depth) {
-	'use strict';
-	depth -= 1;
-	if (depth === this.depth || this.childCircle === null) {
-		return this.radiusPointX;
-	} else {
-		return this.childCircle.getRadiusPointX(depth);
-	}
-};
-
+  'use strict'
+  depth -= 1
+  if (depth === this.depth || this.childCircle === null) {
+    return this.radiusPointX
+  } else {
+    return this.childCircle.getRadiusPointX(depth)
+  }
+}
 MultiCircle.prototype.getRadiusPointY = function (depth) {
-	'use strict';
-	depth -= 1;
-	if (depth === this.depth || this.childCircle === null) {
-		return this.radiusPointY;
-	} else {
-		return this.childCircle.getRadiusPointY(depth);
-	}
-};
-
-
+  'use strict'
+  depth -= 1
+  if (depth === this.depth || this.childCircle === null) {
+    return this.radiusPointY
+  } else {
+    return this.childCircle.getRadiusPointY(depth)
+  }
+}
 MultiCircle.prototype.getX = function () {
-	'use strict';
-	if (this.childCircle === null) {
-		return this.radiusPointX;
-	} else {
-		return this.childCircle.getX();
-	}
-};
-
+  'use strict'
+  if (this.childCircle === null) {
+    return this.radiusPointX
+  } else {
+    return this.childCircle.getX()
+  }
+}
 MultiCircle.prototype.getY = function () {
-	'use strict';
-	if (this.childCircle === null) {
-		return this.radiusPointY;
-	} else {
-		return this.childCircle.getY();
-	}
-};
-
-
-
-
-
-
-
+  'use strict'
+  if (this.childCircle === null) {
+    return this.radiusPointY
+  } else {
+    return this.childCircle.getY()
+  }
+}
 //  END: circleManager
 // ==================================================================
