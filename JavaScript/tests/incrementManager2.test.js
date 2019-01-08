043b4cc36399b567f22a2506da0b5d8b37a4cc79
Evan Wills <evan.wills@acu.edu.au>
2019-01-09 08:08:35 +1100

    convert to StandardJS code style.

--- JavaScript/tests/incrementManager2.test.js
+++ JavaScript/tests/incrementManager2.test.js
@@ -1,211 +1,197 @@
-function report(name, obj, loops) {
-	'use strict';
-	var a = 0,
-		cumulative = 0;
-	console.log("\n\n\n\n\n\n\n\n\n\n\n==========================================\n\n\n\n\n\n\n\n");
-	console.log(name + '.constructor.name = ', obj.constructor.name);
-	console.log(name + ' = ', obj);
-	console.log(obj.constructor.name + ' is instanceof IIncrementManager = ', obj instanceof IIncrementManager);
-	console.log('obj.isCumulative: ', obj.isCumulative);
-	console.log('obj.isInfinite: ', obj.isInfinite);
-	console.log('obj.doesOscillate: ', obj.doesOscillate);
-	console.log('obj.doesReset: ', obj.doesReset);
+function report (name, obj, loops) {
+  'use strict'
+  var a = 0
+  var cumulative = 0
+  console.log('\n\n\n\n\n\n\n\n\n\n\n==========================================\n\n\n\n\n\n\n\n')
+  console.log(name + '.constructor.name = ', obj.constructor.name)
+  console.log(name + ' = ', obj)
+  console.log(obj.constructor.name + ' is instanceof IIncrementManager = ', obj instanceof IIncrementManager)
+  console.log('obj.isCumulative: ', obj.isCumulative)
+  console.log('obj.isInfinite: ', obj.isInfinite)
+  console.log('obj.doesOscillate: ', obj.doesOscillate)
+  console.log('obj.doesReset: ', obj.doesReset)
 
-	if (loops === undefined) {
-		loops = 25;
-	}
-	for (a = 0; a < loops; a += 1) {
-		try {
-			obj.getStep();
-		} catch (e) {
-			console.log(e.message);
-			return;
-		}
+  if (loops === undefined) {
+    loops = 25
+  }
+  for (a = 0; a < loops; a += 1) {
+    try {
+      obj.getStep()
+    } catch (e) {
+      console.log(e.message)
+      return
+    }
 
-		try {
-			obj.updateStep();
-		} catch (e) {
-			console.log(e.message);
-			return;
-		}
+    try {
+      obj.updateStep()
+    } catch (e) {
+      console.log(e.message)
+      return
+    }
 
-		cumulative += obj.getStep();
-		console.log(name + '.getStep() = ', obj.getStep());
-		console.log('cumulative: ', cumulative);
-	}
+    cumulative += obj.getStep()
+    console.log(name + '.getStep() = ', obj.getStep())
+    console.log('cumulative: ', cumulative)
+  }
 }
 
-
-
-
-var fixedInc = new IncrementManager(2.5);
-report('fixedInc', fixedInc);
-
-
+var fixedInc = new IncrementManager(2.5)
+report('fixedInc', fixedInc)
 
 //  END:  Simple
 // ========================================================
 // START: Cumulative
 
+var fixedIncCumInfinite = new IncrementDecoratorCumulative(fixedInc)
+report('IncrementDecoratorCumulative(IncrementManager(2.5))', fixedIncCumInfinite)
 
-
-var fixedIncCumInfinite = new IncrementDecoratorCumulative(fixedInc);
-report('IncrementDecoratorCumulative(IncrementManager(2.5))', fixedIncCumInfinite);
-
-var min = new IncrementManager(0);
-var max = new IncrementManager(10);
+var min = new IncrementManager(0)
+var max = new IncrementManager(10)
 var fixedIncCumReset = new IncrementDecoratorReset(
-	fixedInc,
-	min,
-	max
-);
-report('IncrementDecoratorReset(IncrementManager(2.5), 0, 10)', fixedIncCumReset);
-
+  fixedInc,
+  min,
+  max
+)
+report('IncrementDecoratorReset(IncrementManager(2.5), 0, 10)', fixedIncCumReset)
 
 //  END:  Cumulative
 // ========================================================
 // START: Reset
 
-
-fixedInc = new IncrementManager(2.5);
+fixedInc = new IncrementManager(2.5)
 var fixedIncCumResetPreset = new IncrementDecoratorReset(
-	fixedInc,
-	min,
-	max,
-	0.8
-);
-report('IncrementDecoratorReset(' + fixedInc.getStep() + ', ' + min.getStep() + ', ' + max.getStep() + ', 0.8)', fixedIncCumResetPreset);
+  fixedInc,
+  min,
+  max,
+  0.8
+)
 
-
+report('IncrementDecoratorReset(' + fixedInc.getStep() + ', ' + min.getStep() + ', ' + max.getStep() + ', 0.8)', fixedIncCumResetPreset)
 
 //  END:  Reset
 // ========================================================
 // START: Oscillate
 
-
-
 var fixedIncCumOscillate = new IncrementDecoratorOscillate(
-	fixedInc,
-	min,
-	max
-);
-report('IncrementDecoratorOscillate(' + fixedInc.getStep() + ', ' + min.getStep() + ', ' + max.getStep() + ')', fixedIncCumOscillate);
+  fixedInc,
+  min,
+  max
+)
+
+report('IncrementDecoratorOscillate(' + fixedInc.getStep() + ', ' + min.getStep() + ', ' + max.getStep() + ')', fixedIncCumOscillate)
 
 var fixedIncCumOscillatePreset = new IncrementDecoratorOscillate(
-	fixedInc,
-	min,
-	max,
-	0.8
-);
-report('IncrementDecoratorOscillate(' + fixedInc.getStep() + ', ' + min.getStep() + ', ' + max.getStep() + ', 0.8)', fixedIncCumOscillatePreset);
+  fixedInc,
+  min,
+  max,
+  0.8
+)
 
-
+report('IncrementDecoratorOscillate(' + fixedInc.getStep() + ', ' + min.getStep() + ', ' + max.getStep() + ', 0.8)', fixedIncCumOscillatePreset)
 
 //  END:  Oscillate
 // ========================================================
 // START: Decay
 
+fixedInc = new IncrementManager(10)
 
+var decayInc = new IncrementDecoratorDecay(fixedInc, 0.9908)
 
-fixedInc = new IncrementManager(10);
-var decayInc = new IncrementDecoratorDecay(fixedInc, 0.9908);
-report('decayInc', decayInc);
+report('decayInc', decayInc)
 
-var incCum_IncDecay = new IncrementDecoratorCumulative(
-	new IncrementDecoratorDecay(
-		fixedInc,
-		0.9908
-	)
-);
-report('IncrementDecoratorCumulative(IncrementDecoratorDecay(' + fixedInc.getStep() + ', 0.9908))', incCum_IncDecay);
+var incCumIncDecay = new IncrementDecoratorCumulative(
+  new IncrementDecoratorDecay(
+    fixedInc,
+    0.9908
+  )
+)
 
-
+report('IncrementDecoratorCumulative(IncrementDecoratorDecay(' + fixedInc.getStep() + ', 0.9908))', incCumIncDecay)
 
 //  END:  Decay
 // ========================================================
 // START: Decay multi
 
+fixedInc = new IncrementManager(49)
+var incDecayIncCum = new IncrementDecoratorDecay(
+  new IncrementDecoratorCumulative(
+    fixedInc
+  ),
+  0.9908
+)
 
+report('IncrementDecoratorDecay(IncrementDecoratorCumulative(' + fixedInc.getStep() + '), 0.9908)', incDecayIncCum)
 
-fixedInc = new IncrementManager(49);
-var incDecay_incCum = new IncrementDecoratorDecay(
-	new IncrementDecoratorCumulative(
-		fixedInc
-	),
-	0.9908
-);
-report('IncrementDecoratorDecay(IncrementDecoratorCumulative(' + fixedInc.getStep() + '), 0.9908)', incDecay_incCum);
+max = new IncrementManager(360)
 
-max = new IncrementManager(360);
-var incReset_incDecay = new IncrementDecoratorReset(
-	new IncrementDecoratorDecay(
-		fixedInc,
-		0.9908
-	),
-	min,
-	max
-);
-report('IncrementDecoratorReset(IncrementDecoratorDecay(' + fixedInc.getStep() + ', 0.9908), ' + min.getStep() + ', ' + max.getStep() + ')', incReset_incDecay);
-var incDecay_incReset = new IncrementDecoratorDecay(
-	new IncrementDecoratorReset(
-		fixedInc,
-		min,
-		max
-	),
-	0.9908
-);
-report('IncrementDecoratorDecay(IncrementDecoratorReset(' + fixedInc.getStep() + ', 0.9908), ' + min.getStep() + ', ' + max.getStep() + ')', incDecay_incReset);
+var incResetIncDecay = new IncrementDecoratorReset(
+  new IncrementDecoratorDecay(
+    fixedInc,
+    0.9908
+  ),
+  min,
+  max
+)
 
-var incOscil_incDecay = new IncrementDecoratorOscillate(
-	new IncrementDecoratorDecay(
-		fixedInc,
-		0.9908
-	),
-	min,
-	max
-);
-report('IncrementDecoratorOscillate(IncrementDecoratorDecay(' + fixedInc.getStep() + ', 0.9908), ' + min.getStep() + ', ' + max.getStep() + ')', incOscil_incDecay);
+report('IncrementDecoratorReset(IncrementDecoratorDecay(' + fixedInc.getStep() + ', 0.9908), ' + min.getStep() + ', ' + max.getStep() + ')', incResetIncDecay)
+var incDecayIncReset = new IncrementDecoratorDecay(
+  new IncrementDecoratorReset(
+    fixedInc,
+    min,
+    max
+  ),
+  0.9908
+)
 
+report('IncrementDecoratorDecay(IncrementDecoratorReset(' + fixedInc.getStep() + ', 0.9908), ' + min.getStep() + ', ' + max.getStep() + ')', incDecayIncReset)
 
-var incDecay_incOscil = new IncrementDecoratorDecay(
-	new IncrementDecoratorOscillate(
-		fixedInc,
-		min,
-		max
-	),
-	0.9908
-);
-report('IncrementDecoratorDecay(IncrementDecoratorOscillate(' + fixedInc.getStep() + ', 0.9908), ' + min.getStep() + ', ' + max.getStep() + ')', incDecay_incOscil);
+var incOscilIncDecay = new IncrementDecoratorOscillate(
+  new IncrementDecoratorDecay(
+    fixedInc,
+    0.9908
+  ),
+  min,
+  max
+)
 
+report('IncrementDecoratorOscillate(IncrementDecoratorDecay(' + fixedInc.getStep() + ', 0.9908), ' + min.getStep() + ', ' + max.getStep() + ')', incOscilIncDecay)
 
-var incCum_incOscil_incDecay = new IncrementDecoratorCumulative(
-	new IncrementDecoratorOscillate(
-		new IncrementDecoratorDecay(
-			fixedInc,
-			0.9908
-		),
-		min,
-		max
-	)
-);
-report('IncrementDecoratorCumulative(IncrementDecoratorOscillate(IncrementDecoratorDecay(' + fixedInc.getStep() + ', 0.9908), ' + min.getStep() + ', ' + max.getStep() + '))', incCum_incOscil_incDecay);
+var incDecayIncOscil = new IncrementDecoratorDecay(
+  new IncrementDecoratorOscillate(
+    fixedInc,
+    min,
+    max
+  ),
+  0.9908
+)
 
+report('IncrementDecoratorDecay(IncrementDecoratorOscillate(' + fixedInc.getStep() + ', 0.9908), ' + min.getStep() + ', ' + max.getStep() + ')', incDecayIncOscil)
 
-var incCum_incDecay_incOscil = new IncrementDecoratorCumulative(
-	new IncrementDecoratorDecay(
-		new IncrementDecoratorOscillate(
-			fixedInc,
-			min,
-			max
-		),
-		0.9908
-	)
-);
-report('IncrementDecoratorCumulative(IncrementDecoratorDecay(IncrementDecoratorOscillate(' + fixedInc.getStep() + ', 0.9908), ' + min.getStep() + ', ' + max.getStep() + '))', incCum_incDecay_incOscil);
+var incCumIncOscilIncDecay = new IncrementDecoratorCumulative(
+  new IncrementDecoratorOscillate(
+    new IncrementDecoratorDecay(
+      fixedInc,
+      0.9908
+    ),
+    min,
+    max
+  )
+)
 
+report('IncrementDecoratorCumulative(IncrementDecoratorOscillate(IncrementDecoratorDecay(' + fixedInc.getStep() + ', 0.9908), ' + min.getStep() + ', ' + max.getStep() + '))', incCumIncOscilIncDecay)
 
+var incCumIncDecayIncOscil = new IncrementDecoratorCumulative(
+  new IncrementDecoratorDecay(
+    new IncrementDecoratorOscillate(
+      fixedInc,
+      min,
+      max
+    ),
+    0.9908
+  )
+)
+
+report('IncrementDecoratorCumulative(IncrementDecoratorDecay(IncrementDecoratorOscillate(' + fixedInc.getStep() + ', 0.9908), ' + min.getStep() + ', ' + max.getStep() + '))', incCumIncDecayIncOscil)
 
 //  END:  Decay multi
 // ========================================================
-
-
--- JavaScript/tests/testCircle.js
+++ JavaScript/tests/testCircle.js
@@ -1,97 +1,68 @@
 
 var config = {
-	"angle": {
-		"initial": 120,
-		"step": -2
-	},
-	"pollyLineID": 'polly',
-	"radius": 400,
-	"viewPort": {
-		"svgID": 'svgWrap',
-		"x": 900,
-		"y": 900
-	}
-};
-
-
-function deg2rad(degrees) {
-	'use strict';
-	degrees = degrees * 1;
-	return ((degrees * Math.PI) / 180);
+  'angle': {
+    'initial': 120,
+    'step': -2
+  },
+  'pollyLineID': 'polly',
+  'radius': 400,
+  'viewPort': {
+    'svgID': 'svgWrap',
+    'x': 900,
+    'y': 900
+  }
 }
 
-
-function hardRotate(angle, x, y) {
-	'use strict';
-	var radius = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)),
-		newAngle = Math.atan(y / x) + deg2rad(angle);
-
-	return [Math.cos(newAngle) * radius, Math.sin(newAngle) * radius];
+function deg2rad (degrees) {
+  'use strict'
+  degrees = degrees * 1
+  return ((degrees * Math.PI) / 180)
 }
 
-/*
-function rotate(angle, x, y) {
-	'use strict';
-	var cosA = 0,
-		sinA = 0;
+function hardRotate (angle, x, y) {
+  'use strict'
+  var radius = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2))
+  var newAngle = Math.atan(y / x) + deg2rad(angle)
 
-	angle = deg2rad(angle);
-
-	console.log('angle = ' + angle);
-
-	cosA = Math.cos(angle);
-	sinA = Math.sin(angle);
-
-//	cosA = Math.floor(Math.cos(angle) * 100000) / 100000;
-//	sinA = Math.floor(Math.sin(angle) * 100000) / 100000;
-
-	x = (x * cosA) - (y * sinA);
-	y = (x * sinA) + (y * cosA);
-
-	return [x, y];
-}
-*/
-
-
-function doIt(conf) {
-	'use strict';
-	var a = 0,
-		b = 0,
-		cumulative = 0,
-		offsetX = conf.viewPort.x / 2,
-		offsetY = conf.viewPort.y / 2,
-		points = '',
-		polly = document.getElementById(conf.pollyLineID),
-		svg = document.getElementById(conf.viewPort.svgID),
-		xy = hardRotate(conf.angle.initial, conf.radius, 0);
-
-	svg.setAttribute('width', conf.viewPort.x);
-	svg.setAttribute('height', conf.viewPort.y);
-	svg.setAttribute('viewPort', '0 0 ' + conf.viewPort.x + ' ' + conf.viewPort.y);
-
-	cumulative += conf.angle.initial;
-
-	polly.setAttribute('points', polly.getAttribute('points') + ' ' + (xy[0] + offsetY) + ',' + (xy[1] + offsetY));
-
-	for (a = 0; a < 170; a += 2) {
-		cumulative += conf.angle.step;
-
-		xy = hardRotate(conf.angle.step, xy[0], xy[1]);
-
-		console.log("\nangleStep: " + conf.angle.step, "\ncumulative: " + cumulative, "\nx: " + xy[0] + "\ny: " + xy[1] + "\n");
-
-		polly.setAttribute('points', polly.getAttribute('points') + ' ' + (xy[0] + offsetX) + ',' + (xy[1] + offsetY));
-
-
-		b += 1;
-		if (b > 10) {
-//			sleep(5);
-			console.log("\n\n ======================================= \n\n");
-			b = 0;
-		}
-	}
+  return [Math.cos(newAngle) * radius, Math.sin(newAngle) * radius]
 }
 
+function doIt (conf) {
+  'use strict'
+  var a = 0
+  var b = 0
+  var cumulative = 0
+  var offsetX = conf.viewPort.x / 2
+  var offsetY = conf.viewPort.y / 2
+  // var points = ''
+  var polly = document.getElementById(conf.pollyLineID)
+  var svg = document.getElementById(conf.viewPort.svgID)
+  var xy = hardRotate(conf.angle.initial, conf.radius, 0)
 
-doIt(config);
+  svg.setAttribute('width', conf.viewPort.x)
+  svg.setAttribute('height', conf.viewPort.y)
+  svg.setAttribute('viewPort', '0 0 ' + conf.viewPort.x + ' ' + conf.viewPort.y)
 
+  cumulative += conf.angle.initial
+
+  polly.setAttribute('points', polly.getAttribute('points') + ' ' + (xy[0] + offsetY) + ',' + (xy[1] + offsetY))
+
+  for (a = 0; a < 170; a += 2) {
+    cumulative += conf.angle.step
+
+    xy = hardRotate(conf.angle.step, xy[0], xy[1])
+
+    console.log('\nangleStep: ' + conf.angle.step, '\ncumulative: ' + cumulative, '\nx: ' + xy[0] + '\ny: ' + xy[1] + '\n')
+
+    polly.setAttribute('points', polly.getAttribute('points') + ' ' + (xy[0] + offsetX) + ',' + (xy[1] + offsetY))
+
+    b += 1
+    if (b > 10) {
+      // sleep(5)
+      console.log('\n\n ======================================= \n\n')
+      b = 0
+    }
+  }
+}
+
+doIt(config)
